b86513b95e0a005a6a1343315ac5a4a5d5097632
Chris Henson <chrishenson.net@gmail.com>
2024-02-11 16:22:31 -0500

    isomorphism

--- Theory/Ism.v
+++ Theory/Ism.v
@@ -53,3 +53,46 @@ Section ism_compose.
   Admitted.  
 
 End ism_compose.
+
+(* making this a class makes cetain statements easier *)
+Class Iso {Cat: Category} {A B: object} (f: A ~> B) := {
+  iso_inv  : B ~> A;
+  iso_left : iso_inv ∘ f ≈ id;
+  iso_right: f ∘ iso_inv ≈ id;
+}.
+
+Section iso_properties.
+  Context (Cat: Category).
+  Variables A B C: object.
+  Variable f: A ~> B.
+
+  Lemma iso_unique (I I': Iso f): I.(iso_inv) ≈ I'.(iso_inv).
+  Proof.
+    destruct I as [h h_left h_right].
+    destruct I' as [g g_left g_right].
+    simpl.
+    rewrite <- (id_right h).
+    rewrite <- (id_left g).
+    rewrite <- g_right.
+    rewrite <- h_left.
+    apply assoc.
+  Qed.
+
+  Variable g: B ~> C.
+
+  Lemma inv_compose : Iso f -> Iso g -> Iso (g ∘ f).
+  Proof.
+    intros [finv f_left f_right] [ginv g_left g_right].
+    apply Build_Iso with (finv ∘ ginv).
+    - rewrite <- assoc.
+      rewrite (assoc _ _ ginv). 
+      rewrite g_left.
+      rewrite id_left.
+      assumption.
+    - rewrite <- assoc.
+      rewrite (assoc _ finv _). 
+      rewrite f_right.
+      rewrite id_left.
+      assumption.
+  Qed.
+End iso_properties.
